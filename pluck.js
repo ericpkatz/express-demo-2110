4b2ee6be8a33722514a1ac8ac7cc4b0256dc6a54
Fullstack Academy <fullstack@fullstacks-mbp.home>
2021-10-27 19:09:27 -0400

    another commit

--- pluck.js
+++ pluck.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+const pluck = (arr, key)=> {
+  return arr.map(item => item[key]);
+};
+
+const stooges = [
+  {name: 'moe', age: 40},
+  {name: 'larry', age: 50},
+  {name: 'curly', age: 60}
+];
+
+console.log(pluck(stooges, 'name'));//[ 'moe', 'larry', 'curly' ]
+console.log(pluck(stooges, 'age'));//[ 'moe', 'larry', 'curly' ]
